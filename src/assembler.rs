f7399b0ced2ab38313ffc1d4e2915e457bbcf4fc
Patrick Allen <prallen90@gmail.com>
2017-05-12 00:27:00 -0400

    Add proper assembler & support labels

--- src/assembler.rs
+++ src/assembler.rs
@@ -2,50 +2,160 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::io::prelude::*;
 
+use std::collections::BTreeMap;
+
 // pub fn load_bytecode(filepath: &str) -> Vec<u8> {
 //     let mut file = File::open(filepath);
 //     let mut buffer: Vec<u8> = Vec::new();
 //     let bytecode = file.read_to_end(&buffer);
 // }
 
+#[derive(Debug)]
+struct SourceLine {
+    text: String,
+    lineno: usize,
+    parts: Vec<String>
+}
+
+impl SourceLine  {
+    fn new(line: String, lineno: usize) -> Self {
+        let text = line.clone();
+        SourceLine {
+            text: text,
+            lineno: lineno,
+            parts: line.split(" ").map(|a| a.to_owned()).collect()
+        }
+    }
+    fn is_label(&self) -> bool {
+        self.parts[0].starts_with(".")
+    }
+    fn is_empty(&self) -> bool {
+        self.text == ""
+    }
+    fn byte_count(&self) -> usize {
+        match self.is_label() || self.is_empty() {
+            true => 0,
+            false => (match_instruction(self.parts[0].to_owned()).byte_count + 1) as usize
+        }
+    }
+}
+
+pub struct Assembler {
+    source: Vec<SourceLine>,
+    symbols: BTreeMap<String, usize>,
+}
+
+impl Assembler {
+    pub fn new(filename: &str) -> Self {
+        let mut sourcelines: Vec<SourceLine> = vec![];
+        for (i, line) in read_lines(filename).to_vec().into_iter().enumerate() {
+            sourcelines.push(SourceLine::new(line.to_string(), i + 1));
+        }
+        Assembler {
+            source: sourcelines,
+            symbols: BTreeMap::new(),
+        }
+    }
+    pub fn assemble(&mut self) -> Vec<u8> {
+        let mut bytecode: Vec<u8> = Vec::new();
+        self.record_labels();
+        for sl in &self.source {
+            let mut bytes = self.parse_line(sl, bytecode.len());
+            bytecode.append(&mut bytes);
+        }
+        bytecode
+    }
+    fn record_labels(&mut self) {
+        let mut bytecount = 0;
+        for sl in &self.source {
+            if sl.is_label() {
+                let label = sl.parts[0].clone();
+                self.symbols.insert(label, bytecount);
+            }
+            bytecount += sl.byte_count();
+        }
+    }
+    fn parse_line(&self, sourceline: &SourceLine, current_len: usize) -> Vec<u8> {
+        let byte_count = sourceline.byte_count();
+        let addr = current_len + byte_count;
+        if byte_count > 0 {
+            let action = sourceline.parts[0].clone();
+            let act2 = action.clone();
+            let instruction = match_instruction(action);
+            let mut tail: Vec<u8> = vec!(instruction.code);
+            if byte_count == 1 {
+                return tail;
+            }
+            let operand = sourceline.parts[1].clone();
+            if operand.starts_with("."){
+                if instruction.lbl == true {
+                    let mut tmp = match self.symbols.get(&operand) {
+                        Some(value) => {
+                            if act2.contains("rel") {
+                                let rel = -((addr as i32) - (*value as i32));
+                                val_to_bytes(rel)
+                            } else {
+                                val_to_bytes(*value as i32)
+                            }
+                        },
+                        None => panic!("{} not a valid label.", operand),
+                    };
+                    tail.append(&mut tmp);
+                } else {
+                    panic!("Instruction {:02X} cannot use a label.", instruction.code);
+                }
+            } else {
+                let mut tmp = match operand.parse() {
+                    Ok(val) => val_to_bytes(val),
+                    Err(_) => panic!("Error, bitch.")
+                };
+                tail.append(&mut tmp);
+            }
+            tail
+        } else {
+            Vec::new()
+        }
+    }
+}
 struct ASMCont {
     code: u8,
     byte_count: u8,
+    lbl: bool,
 }
 fn match_instruction(inst: String) -> ASMCont {
     match &*inst.to_lowercase() {
-        "noop"        => ASMCont {code: 0x00, byte_count: 0},
-        "const"       => ASMCont {code: 0x10, byte_count: 4},
-        "load"        => ASMCont {code: 0x11, byte_count: 4},
-        "g_load"      => ASMCont {code: 0x12, byte_count: 4},
-        "store"       => ASMCont {code: 0x14, byte_count: 4},
-        "g_store"     => ASMCont {code: 0x15, byte_count: 4},
-        "call"        => ASMCont {code: 0x18, byte_count: 4},
-        "add"         => ASMCont {code: 0x40, byte_count: 0},
-        "sub"         => ASMCont {code: 0x41, byte_count: 0},
-        "mul"         => ASMCont {code: 0x42, byte_count: 0},
-        "div"         => ASMCont {code: 0x43, byte_count: 0},
-        "pow"         => ASMCont {code: 0x44, byte_count: 0},
-        "mod"         => ASMCont {code: 0x45, byte_count: 0},
-        "shl"         => ASMCont {code: 0x50, byte_count: 0},
-        "shr"         => ASMCont {code: 0x51, byte_count: 0},
-        "and"         => ASMCont {code: 0x52, byte_count: 0},
-        "or"          => ASMCont {code: 0x53, byte_count: 0},
-        "xor"         => ASMCont {code: 0x54, byte_count: 0},
-        "not"         => ASMCont {code: 0x55, byte_count: 0},
-        "cmp_eq"      => ASMCont {code: 0x61, byte_count: 0},
-        "cmp_ne"      => ASMCont {code: 0x62, byte_count: 0},
-        "cmp_gt"      => ASMCont {code: 0x63, byte_count: 0},
-        "cmp_lt"      => ASMCont {code: 0x64, byte_count: 0},
-        "jmp_rel"     => ASMCont {code: 0x80, byte_count: 4},
-        "jmp_rel_eq"  => ASMCont {code: 0x81, byte_count: 4},
-        "jmp_rel_ne"  => ASMCont {code: 0x82, byte_count: 4},
-        "jmp_rel_gt"  => ASMCont {code: 0x83, byte_count: 4},
-        "jmp_rel_lt"  => ASMCont {code: 0x84, byte_count: 4},
-        "jmp"         => ASMCont {code: 0x88, byte_count: 4},
-        "ret"         => ASMCont {code: 0xA0, byte_count: 0},
-        "print"       => ASMCont {code: 0xE0, byte_count: 0},
-        "halt"        => ASMCont {code: 0xF0, byte_count: 0},
+        "noop"        => ASMCont {code: 0x00, byte_count: 0, lbl: false},
+        "const"       => ASMCont {code: 0x10, byte_count: 4, lbl: false},
+        "load"        => ASMCont {code: 0x11, byte_count: 4, lbl: true},
+        "g_load"      => ASMCont {code: 0x12, byte_count: 4, lbl: true},
+        "store"       => ASMCont {code: 0x14, byte_count: 4, lbl: true},
+        "g_store"     => ASMCont {code: 0x15, byte_count: 4, lbl: true},
+        "call"        => ASMCont {code: 0x18, byte_count: 4, lbl: false},
+        "add"         => ASMCont {code: 0x40, byte_count: 0, lbl: false},
+        "sub"         => ASMCont {code: 0x41, byte_count: 0, lbl: false},
+        "mul"         => ASMCont {code: 0x42, byte_count: 0, lbl: false},
+        "div"         => ASMCont {code: 0x43, byte_count: 0, lbl: false},
+        "pow"         => ASMCont {code: 0x44, byte_count: 0, lbl: false},
+        "mod"         => ASMCont {code: 0x45, byte_count: 0, lbl: false},
+        "shl"         => ASMCont {code: 0x50, byte_count: 0, lbl: false},
+        "shr"         => ASMCont {code: 0x51, byte_count: 0, lbl: false},
+        "and"         => ASMCont {code: 0x52, byte_count: 0, lbl: false},
+        "or"          => ASMCont {code: 0x53, byte_count: 0, lbl: false},
+        "xor"         => ASMCont {code: 0x54, byte_count: 0, lbl: false},
+        "not"         => ASMCont {code: 0x55, byte_count: 0, lbl: false},
+        "cmp_eq"      => ASMCont {code: 0x61, byte_count: 0, lbl: false},
+        "cmp_ne"      => ASMCont {code: 0x62, byte_count: 0, lbl: false},
+        "cmp_gt"      => ASMCont {code: 0x63, byte_count: 0, lbl: false},
+        "cmp_lt"      => ASMCont {code: 0x64, byte_count: 0, lbl: false},
+        "jmp_rel"     => ASMCont {code: 0x80, byte_count: 4, lbl: true},
+        "jmp_rel_eq"  => ASMCont {code: 0x81, byte_count: 4, lbl: true},
+        "jmp_rel_ne"  => ASMCont {code: 0x82, byte_count: 4, lbl: true},
+        "jmp_rel_gt"  => ASMCont {code: 0x83, byte_count: 4, lbl: true},
+        "jmp_rel_lt"  => ASMCont {code: 0x84, byte_count: 4, lbl: true},
+        "jmp"         => ASMCont {code: 0x88, byte_count: 4, lbl: true},
+        "ret"         => ASMCont {code: 0xA0, byte_count: 0, lbl: false},
+        "print"       => ASMCont {code: 0xE0, byte_count: 0, lbl: false},
+        "halt"        => ASMCont {code: 0xF0, byte_count: 0, lbl: false},
         _ => panic!("{} is not a valid instruction", inst)
     }
 }
