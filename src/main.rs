e6d77900d3e689d23049cee0f5aa188739ff8bbd
Patrick Allen <prallen90@gmail.com>
2017-05-14 02:31:00 -0400

    Add log4rs for debug/logging.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+
 use std::env;
 
 mod vm;
 mod stack;
+mod opcode;
 mod program;
 mod assembler;
 mod instruction;
@@ -12,6 +17,8 @@ use vm::VirtualMachine;
 
 
 fn main() {
+    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let mut assembler = Assembler::new(filename);
